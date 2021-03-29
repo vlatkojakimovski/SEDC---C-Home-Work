88b8ba04a7e81dbe9da703c46db902fcdab43baa
Vlatko Jakimovski <jakimovski_vlatko@yahoo.com>
2021-03-29 23:27:04 +0200

    Home work Class-02

---
+++ Class02 - HomeWork/Task-02/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Task_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number for variable A ");
+            string variableA = Console.ReadLine();
+            Console.WriteLine("Please enter number for variable B ");
+            string variableB = Console.ReadLine();
+            Console.WriteLine("Please enter number for variable C ");
+            string variableC = Console.ReadLine();
+            Console.WriteLine("Please enter number for variable D ");
+            string variableD = Console.ReadLine();
+            bool successfulConversionA = double.TryParse(variableA, out double dblVariableA);
+            bool successfulConversionB = double.TryParse(variableB, out double dblVariableB);
+            bool successfulConversionC = double.TryParse(variableC, out double dblVariableC);
+            bool successfulConversionD = double.TryParse(variableD, out double dblVariableD);
+
+            Console.WriteLine("Average number is: " + ((dblVariableA + dblVariableB + dblVariableC + dblVariableD) / 4));
+        }
+    }
+}
---
+++ Class02 - HomeWork/Task-06/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Task_06
+{
+    class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter number for variable A ");
+            string variableA = Console.ReadLine();
+            Console.WriteLine("Please enter number for variable B ");
+            string variableB = Console.ReadLine();
+            bool successfulConversionA = int.TryParse(variableA, out int intVariableA);
+            bool successfulConversionB = int.TryParse(variableB, out int intVariableB);
+
+            if (successfulConversionA && successfulConversionB)
+            {
+                largeNumber(intVariableA, intVariableB);
+            }
+
+        }
+        static int largeNumber(int a, int b)
+        {
+            if (a > b)
+            {
+                Console.WriteLine("Number A:" + a + "is larger then number B:" + b);
+                testEvenOdd(a);
+                return a;
+            }
+            else if (b > a)
+            {
+                Console.WriteLine("Number B:" + b + "is larger then number A:" + a);
+                testEvenOdd(b);
+                return b;
+            }
+            else
+            {
+                Console.WriteLine("The numbers are equal: " + a + " = " + b);
+                return 0;
+            }
+        }
+
+        static void testEvenOdd(int a)
+        {
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("The number " + a + " is EVEN");
+            }
+            else
+            {
+                Console.WriteLine("The number " + a + " is ODD");
+            }
+        }
+
+
+
+    }
+}
