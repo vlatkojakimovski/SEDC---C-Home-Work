23bd20692eba113c820687c1bd8dfc5c8b07dfc9
Vlatko Jakimovski <jakimovski_vlatko@yahoo.com>
2021-04-13 11:56:59 +0200

    Class 08 - Homework

---
+++ Class08 - HomeWork/Homework Class08/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Homework_Class08
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Queue<int> queueNumbers = new Queue<int>();
+            Stack<int> stackNumbers = new Stack<int>();
+            List<int> listNumbers = new List<int>();
+
+            while (true)
+            {
+                Console.WriteLine("Enter the number");
+                string anotherNumber = "";
+                bool successfullParsed = int.TryParse(Console.ReadLine(), out int parsedNumber);
+                if (successfullParsed)
+                {
+                    queueNumbers.Enqueue(parsedNumber);
+                    stackNumbers.Push(parsedNumber);
+                    listNumbers.Add(parsedNumber);
+
+                    Console.WriteLine("Enter Y if you want to add another number");
+                    anotherNumber = Console.ReadLine();
+                } else
+                {
+                    Console.WriteLine("Wrong input !!!");
+                    Console.WriteLine("Enter Y if you want to add another number");
+                    anotherNumber = Console.ReadLine();
+                }
+                if (anotherNumber.ToLower() != "y")
+                    break;
+            }
+
+            stackNumbers = StackTransforming(stackNumbers);
+
+            PrintCollection(queueNumbers);
+            PrintCollection(stackNumbers);
+            PrintCollection(listNumbers);
+
+        }
+
+        static void PrintCollection(IEnumerable collection)
+        {
+            foreach (object item in collection)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("\n");
+        }
+
+        static Stack<int> StackTransforming (Stack<int> st)
+        {
+            Stack<int> tempStack = new Stack<int>();
+            foreach (int item in st)
+            {
+                tempStack.Push(item);
+            }
+            return tempStack;
+        }
+    }
+}
