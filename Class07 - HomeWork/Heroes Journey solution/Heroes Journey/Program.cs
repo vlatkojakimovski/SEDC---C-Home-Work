f06baaad07748dd769e3f1d4020193aac0541213
Vlatko Jakimovski <jakimovski_vlatko@yahoo.com>
2021-04-17 08:50:12 +0200

    Heroes journey updates

--- Class07 - HomeWork/Heroes Journey solution/Heroes Journey/Program.cs
+++ Class07 - HomeWork/Heroes Journey solution/Heroes Journey/Program.cs
@@ -7,9 +7,16 @@ namespace Heroes_Journey
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            YellowConsoleColor();
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("<><><><><><><> Heroes Journey <><><><><><><>".Length / 2)) + "}", "<><><><><><> Heroes Journey<> <><><><><>"));
+            GreenConsoleColor();
+            Console.WriteLine(String.Format("{0," + ((Console.WindowWidth / 2) + ("<><><><><><>  WELCOME!  <><><><><><>".Length / 2)) + "}", "<><><><><><>  WELCOME!  <><><><><><>"));
 
-            User [] UsersArr = { new User("vlatko@yahoo.com", "123456"), new User("jakimovski@uahoo.com", "654321") };
+
+            //Console.WriteLine(String.Format( + Console.WindowWidth +, "<><><><><><>WELCOME!<><><><><><>!"));
+
+
+            User[] UsersArr = { new User("vlatko@yahoo.com", "123456"), new User("jakimovski@uahoo.com", "654321") };
             if (!EmailPasswordCorrect(UsersArr))
             {
                 Console.WriteLine("You have entered 5 wrong inputs, App is closing");
@@ -22,13 +29,14 @@ namespace Heroes_Journey
         /// START GAME
         static void StartGame()
         {
-
+            YellowConsoleColor();
             Hero heroOne = new Hero();
-            Console.WriteLine("Next STEP -> Caharacter creating \n Enter character name :");
+            Console.WriteLine("Next STEP -> Caharacter creating \n Enter your character name :");
 
             heroOne.Name = NameInput();
 
             ChooseRace(heroOne);
+            BlueConsoleColor();
             ChooseClass(heroOne);
 
             int EventCounter = 0;
@@ -52,7 +60,7 @@ namespace Heroes_Journey
             bool LogIn = false;
             while (counter < 5)
             {
-                Console.WriteLine("Please insert Email");
+                Console.WriteLine("Enter Email");
                 email = Console.ReadLine();
                 Console.WriteLine("Please insert Password");
                 pass = Console.ReadLine();
@@ -94,7 +102,7 @@ namespace Heroes_Journey
                 {
                     return name;
                 }
-                Console.WriteLine("Please write name with more then 1 and less then 20 characters");
+                Console.WriteLine("Please write name of your CHARACTER with more then 1 and less then 20 characters");
             }
             return name;
         }
@@ -102,10 +110,10 @@ namespace Heroes_Journey
         //SWITCH  RACES  AND  CLASS 
         static void ChooseRace (Hero hero)
         {
-            Console.WriteLine("Select Race 1,  2  or  3");
-            Console.WriteLine("1) Dwarf \n - Has 100 Health \n - Has 6 Strength \n - Has 2 Agility \n");
-            Console.WriteLine("2) Elf \n - Has 60 Health \n - Has 4 Strength \n - Has 6 Agility \n");
-            Console.WriteLine("3) Human \n - Has 80 Health \n - Has 5 Strength \n - Has 4 Agility \n");
+            Console.WriteLine("Select Race :");
+            Console.WriteLine("1) Dwarf (Has 100 Health  , Has 6 Strength , Has 2 Agility )");
+            Console.WriteLine("2) Elf ( Has 60 Health , Has 4 Strength , Has 6 Agility )");
+            Console.WriteLine("3) Human ( Has 80 Health , Has 5 Strength , Has 4 Agility ) \n");
             string choice = Console.ReadLine();
 
             switch (choice)
@@ -120,7 +128,7 @@ namespace Heroes_Journey
                     hero.Health = 80; hero.Strength = 5; hero.Agility = 4; hero.Race = "Human";
                     break;
                 default:
-                    Console.WriteLine("Wrong select !!! ");
+                    Console.WriteLine("Wrong select !!! \n");
                     ChooseRace(hero);
                     break;
             }
@@ -131,10 +139,10 @@ namespace Heroes_Journey
             string choice = "-1" ;
             while (choice != "1" || choice != "2" || choice != "3")
             {
-                Console.WriteLine("Select Class 1,  2  or  3");
-                Console.WriteLine("1) Warrior \n * + 20 Health \n * - 1 Agility \n");
-                Console.WriteLine("2) Rogue \n * - 20 Health \n * + 1 Agility \n");
-                Console.WriteLine("3) Mage \n * + 20 Health \n * - 1 Strength \n");
+                Console.WriteLine("Select Class :");
+                Console.WriteLine("1) Warrior ( + 20 Health , - 1 Agility )");
+                Console.WriteLine("2) Rogue  ( - 20 Health , + 1 Agility )");
+                Console.WriteLine("3) Mage ( + 20 Health , - 1 Strength )");
 
                 choice = Console.ReadLine();
                 if (choice == "1" || choice == "2" || choice == "3")
@@ -180,6 +188,30 @@ namespace Heroes_Journey
             return number.Next(1, 11);
         }
 
+        static void FightCalculate(Hero hero, int healthSubstraction)
+        {
+            BlueConsoleColor();
+            hero.Health -= healthSubstraction;
+            Console.WriteLine($"YOU LOST THE FIGHT! Substract {healthSubstraction} and now you have {hero.Health} HP");
+            if (hero.Health <= 0)
+            {
+                Console.WriteLine("YOU LOST, YOU ARE DEAD ! \n Dou you want to start new game ? \n 1) YES \n 2) NO");
+                string choiceAfterLost = Console.ReadLine();
+                while (choiceAfterLost != "1" && choiceAfterLost != "2")
+                {
+                    Console.WriteLine("You must choose  \n 1 - New Game  \n 2 - Exit application");
+                    choiceAfterLost = Console.ReadLine();
+                }
+                if (choiceAfterLost == "1")
+                {
+                    StartGame();
+                }
+                else
+                {
+                    System.Environment.Exit(0);
+                }
+            }            
+        }
 
         static void ChooseFightOrRunAway (Hero hero)
         {
@@ -187,36 +219,20 @@ namespace Heroes_Journey
             string choice = "-1";
             while (choice != "1" && choice != "2")
             {
-                Console.WriteLine("Choose event: \n 1) FIGHT !!! \n 2) Run Away !");
+                RedConsoleColor();
+                Console.WriteLine("Choose event: \n 1) FIGHT !!! \n 2) Run Away ! \n");
                 choice = Console.ReadLine();
             }
-            if(choice == "1")
+            if (choice == "1")
             {
                 if (RandomNumberOneToTen() < hero.Strength)
                 {
+                    GreenConsoleColor();
                     Console.WriteLine("YOU WON FIGHT !!!");
-                } else
+                }
+                else
                 {
-                    hero.Health -= healthSubstraction;
-                    Console.WriteLine($"YOU LOST THE FIGHT! Substract {healthSubstraction} and now you have {hero.Health} HP");
-                    if (hero.Health <= 0)
-                    {
-                        Console.WriteLine("YOU LOST, YOU ARE DEAD ! \n Dou you want to start new game ? \n 1) YES \n 2) NO");
-                        string choiceAfterLost = Console.ReadLine();
-                        while (choiceAfterLost != "1" && choiceAfterLost != "2" )
-                        {
-                            Console.WriteLine("You must choose  \n 1 - New Game  \n 2 - Exit application");
-                            choiceAfterLost = Console.ReadLine();
-                        }
-                        if (choiceAfterLost == "1")
-                        {
-                            StartGame();
-                        }
-                        else
-                        {
-                            System.Environment.Exit(0);
-                        }
-                    }
+                    FightCalculate(hero, healthSubstraction);
                 }
             }
             else if (choice == "2")
@@ -227,26 +243,7 @@ namespace Heroes_Journey
                 }
                 else
                 {
-                    hero.Health -= healthSubstraction;
-                    Console.WriteLine($"YOU GOT COUGHT ! Substract {healthSubstraction} and now you have {hero.Health} HP");
-                    if (hero.Health <= 0)
-                    {
-                        Console.WriteLine("YOU LOST, YOU ARE DEAD ! \n Dou you want to start new game ? \n 1) YES \n 2) NO");
-                        string choiceAfterLost = Console.ReadLine();
-                        while (choiceAfterLost != "1" && choiceAfterLost != "2")
-                        {
-                            Console.WriteLine("You must choose  \n 1 - New Game  \n 2 - Exit application");
-                            choiceAfterLost = Console.ReadLine();
-                        }
-                        if (choiceAfterLost == "1")
-                        {
-                            StartGame();
-                        }
-                        else
-                        {
-                            System.Environment.Exit(0);
-                        }
-                    }
+                    FightCalculate(hero, healthSubstraction);
                 }
             }
         }
@@ -254,27 +251,28 @@ namespace Heroes_Journey
         // Event involved in and health substraction return
         static int EventInvolved()
         {
+            BlueConsoleColor();
             int healthSubstract = 0;
             switch (RandomNumberOneToFive())
             {
                 case 1:
-                    Console.WriteLine("Bandits attack you out of nowhere. They seem very dangerous... \n  * -20 health");
+                    Console.WriteLine("Bandits attack you out of nowhere. They seem very dangerous... \n  * -20 health \n");
                     healthSubstract = 20;
                     break;
                 case 2:
-                    Console.WriteLine("You bump in to one of the guards of the nearby village. They attack you without warning... \n * - 30 health");
+                    Console.WriteLine("You bump in to one of the guards of the nearby village. They attack you without warning... \n * - 30 health \n");
                     healthSubstract = 30;
                     break;
                 case 3:
-                    Console.WriteLine("A Land Shark appears. It starts chasing you down to eat you... \n * - 50 health");
+                    Console.WriteLine("A Land Shark appears. It starts chasing you down to eat you... \n * - 50 health \n");
                     healthSubstract = 50;
                     break;
                 case 4:
-                    Console.WriteLine("You accidentally step on a rat. His friends are not happy. They attack... \n * - 10 health");
+                    Console.WriteLine("You accidentally step on a rat. His friends are not happy. They attack... \n * - 10 health \n");
                     healthSubstract = 10;
                     break;
                 case 5:
-                    Console.WriteLine("You find a huge rock. It comes alive somehow and tries to smash you... \n * - 30 health");
+                    Console.WriteLine("You find a huge rock. It comes alive somehow and tries to smash you... \n * - 30 health \n");
                     healthSubstract = 30;
                     break;
                 default:
@@ -283,7 +281,28 @@ namespace Heroes_Journey
             return healthSubstract;
         }
 
-        
+        // CONSOLE COLOR 
+        static void YellowConsoleColor ()
+        {
+            Console.ForegroundColor = ConsoleColor.Yellow;
+        }
+
+        static void BlueConsoleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+
+        static void GreenConsoleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Green;
+        }
+
+        static void RedConsoleColor()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+
+
     }
 
 }
